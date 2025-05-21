47737f512684fc6b8938879f081cff39fac90ebf
baleunbit <vahyeon2514@sonline20.sen.go.kr>
2025-05-21 11:39:46 +0900

    39. 씬 전환

--- Assets/1_Scene/2_Scripts/FinishLine.cs
+++ Assets/1_Scene/2_Scripts/FinishLine.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class FinishLine : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("완주했습니다!");
+            SceneManager.LoadScene(0);
         }
     }
 }
